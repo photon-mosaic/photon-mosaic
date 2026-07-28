8d0e5197eadb611fc974f3f97d6d715a3281b0cb
alessandrofelder <alessandrofelder@users.noreply.github.com>
2026-07-28 15:03:15 +0100

    fix order of imports in conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 """Configuration file for the Sphinx documentation builder."""
 
-from pathlib import Path
 import sys
+from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]
 SRC = ROOT / "src"
